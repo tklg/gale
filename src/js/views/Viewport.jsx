d50db7b35f0f806b5ac98b41b6d293cc12ae2adc
Theodore Kluge <villa7.kluge5@gmail.com>
2017-04-16 18:51:58 -0400

    fix search tags

--- src/js/views/Viewport.jsx
+++ src/js/views/Viewport.jsx
@@ -31,6 +31,7 @@ export default class Viewport extends React.Component {
 			activeFolderID: '', // ALL
 			folders: [],
 			tags: [],
+			prevTags: [],
 			searchTags: [],
 			searchActive: null,
 			itemsPerPage: 40,
@@ -181,7 +182,7 @@ export default class Viewport extends React.Component {
 		if (index != this.state.activeFolder || force) changed = true;
 		var cur = this.state.files;
 		
-		if (index == -1 || (index != null && !!id) || changed) {
+		if (index == -1 || (index != null && !!id) || changed || (!tags.equals(this.state.prevTags))) {
 			cur = [];
 			this.setState({
 				currentOffset: 0
@@ -645,3 +646,33 @@ const ajax = opts => {
 	}
 	queue.add(enq);
 }
+
+// http://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
